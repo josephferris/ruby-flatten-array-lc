099566747471ce67b0e28cc059da7fa3ca91217b
Joey Ferris <joey@joeyferris.io>
2019-04-22 20:15:36 -0500

    Initial commit

---
+++ flatten-array.rb
@@ -0,0 +1,15 @@
+#
+#  1. Create a function to flatten the below array by one dimension.
+#
+#     oneDimensionalArray = [1, [2, 3, [4, 5]]]
+#
+#
+#  2. Create a function to flatten the below array n times.
+#
+#     nTimesArray = [1, [2, 3, [4, [5, 6]]]]
+#
+#
+#  3. Create a function to flatten the below array using recursion.
+#
+#     arrayWithRecursion = [1, [2, 3, [4, [5, 6]]]]
+#
